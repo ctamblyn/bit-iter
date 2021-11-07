1e13ab1ac847577eea0518cf1c8050eeb5efe1e4
Christian Tamblyn <ctamblyn76@gmail.com>
2021-11-07 22:55:22 +0000

    Bump version to 1.0.0.

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/bit-iter/0.1.6")]
+#![doc(html_root_url = "https://docs.rs/bit-iter/1.0.0")]
 
 use core::{iter::FusedIterator, mem::size_of};
 
