e76c64f4570663a3b76a1299b29f81ac7478e934
Christian Tamblyn <ctamblyn76@gmail.com>
2021-11-12 18:41:53 +0000

    Bump version to 1.1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/bit-iter/1.0.0")]
+#![doc(html_root_url = "https://docs.rs/bit-iter/1.1.0")]
 
 use core::{
     iter::{ExactSizeIterator, FusedIterator},
